c40275cb67cd864ee78f60ff8c0a7b7ee1180218
kalvin chen <kalvin412@gmail.com>
2024-08-06 18:47:26 +0800

    implemented GA

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import SA_workshift as SA
+import GA_workshift as GA
 from helpers.helper import print_schedule
 import random
 
 # for debugging perpose
 random.seed(1)
 
-solution = SA.run()
-print_schedule(solution.state)
+# solution = SA.run()
+# print_schedule(solution.state)
+
+solution = GA.run()
+
+print_schedule(solution.genes)
